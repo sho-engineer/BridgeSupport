4babd8caf8f99b2017b37951d700f4576d627dd3
sho-engineer <arlonbolt@gmail.com>
2022-12-19 23:46:09 +0900

    Fix Credentials Authentication setup

--- bridge-support-next/pages/api/auth/[...nextauth].js
+++ bridge-support-next/pages/api/auth/[...nextauth].js
@@ -1,9 +1,12 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import { CredentialsProvider } from 'next-auth/providers';
 
-const { PrismaClient } = require('@prisma/client')
-const prisma  = new PrismaClient();
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const isPasswordConfirmOk = (pw, pwConfirm) => {
   /**
@@ -21,13 +24,40 @@ const isPasswordConfirmOk = (pw, pwConfirm) => {
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    // Sign in with Github account
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    // Sign in with Google account
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name:"サインイン",
+      id:"credential",
+      type:"credentials",
+      credentials:{
+        username: { label: "ユーザー名", type: "text", placeholder: "ユーザー名" },
+        password: {  label: "パスワード", type: "password" },
+        passwordConfirmation: {  label: "パスワード確認", type: "password" }
+      },
+      authorize (credentials, req) {
+        const { username, password, passwordConfirmation } = credentials;
+        let user;
+        if (isPasswordConfirmOk(password, passwordConfirmation)) {
+          user = {
+            name: username,
+            password: password
+          }
+        }
+        if(user){
+          return user
+        }else{
+          return null
+        }
+      }
     }),
   ],
 });
