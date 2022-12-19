806b28f6621845c544a5ddca85a15069a5163e3a
sho-engineer <arlonbolt@gmail.com>
2022-12-19 23:37:37 +0900

    Add logic to check pw and pwConfirm are same

--- bridge-support-next/pages/api/auth/[...nextauth].js
+++ bridge-support-next/pages/api/auth/[...nextauth].js
@@ -5,6 +5,19 @@ import GoogleProvider from 'next-auth/providers/google';
 const { PrismaClient } = require('@prisma/client')
 const prisma  = new PrismaClient();
 
+const isPasswordConfirmOk = (pw, pwConfirm) => {
+  /**
+   * 入力されたパスワードと確認用パスワードが同一であるか判定
+   * param:pw string パスワード
+   * param:pwConfirm string 確認用パスワード
+   * return: true/false bool 
+   */
+  if (pw === pwConfirm) {
+    return true
+  }
+  return false
+}
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
