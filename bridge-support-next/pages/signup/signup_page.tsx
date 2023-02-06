cf2d0a0c770cb7512fcceef1e8eff428c195e5ee
sho-engineer <arlonbolt@gmail.com>
2023-02-06 23:35:35 +0900

    fix 各種テキストボックスがregisterの対象となるように修正

--- bridge-support-next/pages/signup/signup_page.tsx
+++ bridge-support-next/pages/signup/signup_page.tsx
@@ -4,6 +4,7 @@ import { registerAction } from './signup.hooks';
 import Link from 'next/link';
 import {
   Card,
+  Input,
   Spacer,
   Button,
   Text,
@@ -12,7 +13,6 @@ import {
   Container,
 } from '@nextui-org/react';
 import { SubmitHandler, useForm } from "react-hook-form";
-import InputItem from '../../components/InputItem';
 import { Toaster } from 'react-hot-toast';
 import { printError } from "../utils";
 
@@ -23,15 +23,31 @@ type ValuesType =  {
   passwordConfirmation: string;
 }
 
-const handleOnSubmit: SubmitHandler<ValuesType> = (data) => {
-  console.log(data);
-}
+// テキストボックス
+const listItems: ValuesType = {
+  "userName": "名前",
+  "email": "Eメール",
+  "password": "パスワード",
+  "passwordConfirmation": "パスワード確認用"
+} 
+
+// 配列のキー名
+const items = [
+  "userName",
+  "email",
+  "password",
+  "passwordConfirmation"
+]
 
 const Signup = () => {
   const { 
     register, 
     handleSubmit, 
   } = useForm<ValuesType>();
+
+  const handleOnSubmit: SubmitHandler<ValuesType> = (data: ValuesType) => {
+    alert(JSON.stringify(data));
+  }
 
   return (
     <div>
@@ -54,26 +70,41 @@ const Signup = () => {
             BridgeSupport サインアップ
           </Text>
           <Spacer y={1} />
-          <form onSubmit={handleSubmit(handleOnSubmit)}
-          >
-
-          <InputItem placeholder='名前' { ...register('userName')} />
-          <InputItem placeholder='Eメール' { ...register('email')} />
-          <InputItem placeholder='パスワード' { ...register('password')} />
-          <InputItem placeholder='パスワード確認用' { ...register('passwordConfirmation')} />
-
-          <Row justify="space-between">
-            <Checkbox>
-              <Text size={14}>ログイン状態を保存する</Text>
-            </Checkbox>
-            <Link 
-              href="/login/login_page"
-              >
-              ログイン画面へ
-            </Link>
-          </Row>
-          <Spacer y={1} />
-          <Button type='submit'>ログイン</Button>
+          <form onSubmit={handleSubmit(handleOnSubmit)}>
+            {
+              items.map((data) => {
+                let elem = listItems[data];
+                return(
+                  <div> 
+                    <Input clearable underlined fullWidth color="primary" size="lg"
+                      placeholder={elem} css={{ mb: '6px' }} 
+                      {...register(`${elem}`)}
+                      />
+                    <Spacer y={1} />
+                  </div>
+                )
+              })
+            }
+            <Row justify="space-between">
+              <Checkbox>
+                <Text size={14}>ログイン状態を保存する</Text>
+              </Checkbox>
+              <Link 
+                href="/login/login_page"
+                >
+                ログイン画面へ
+              </Link>
+            </Row>
+            <Spacer y={1} />
+            <Button 
+              type='submit'
+              css={{
+                margin: '0 auto',
+                width: '50%',
+              }}
+            >
+            ログイン
+            </Button>
           </form>
         </Card>
       </Container>
